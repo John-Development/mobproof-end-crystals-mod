33807d1a127f15cf6ab6541aa7bd33feeecf8ed1
John-Development <38006800+John-Development@users.noreply.github.com>
2020-11-28 13:32:46 +0100

    First version
    1.0.0

---
+++ src/main/java/net/mobProofCrystals/mobProofCrystals.java
@@ -0,0 +1,10 @@
+package net.mobProofCrystals;
+
+import net.fabricmc.api.ModInitializer;
+
+public class mobProofCrystals implements ModInitializer {
+  
+  @Override
+  public void onInitialize() {
+  }
+}
