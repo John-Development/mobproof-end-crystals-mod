24040bc45dcf9545ef8172c545804b879e42ba64
John-Development <38006800+John-Development@users.noreply.github.com>
2021-02-24 19:44:34 +0100

    Moved config file creation to PropertiesCache

--- src/main/java/net/mobProofCrystals/mobProofCrystals.java
+++ src/main/java/net/mobProofCrystals/mobProofCrystals.java
@@ -1,31 +1,19 @@
 package net.mobProofCrystals;
 
-import java.io.File;
 import java.io.IOException;
 
 
 import net.fabricmc.api.ModInitializer;
 import net.mobProofCrystals.util.PropertiesCache;
 
-import static net.mobProofCrystals.util.PropertiesCache.CONFIG_FILE;
-
 public class mobProofCrystals implements ModInitializer {
 
   final public static String DEF_RAD = "32";
   final public static String DEF_LIM_DISTANCE = "1";
   final public static String DEF_CRYSTAL_NAME = "";
-  final public static String DEPRECATED_CONFIG_FILE = "crystal.properties";
   @Override
   public void onInitialize() {
     try {
-      File deprecatedConfigFile = new File(DEPRECATED_CONFIG_FILE);
-      if (deprecatedConfigFile.isFile()) {
-        deprecatedConfigFile.renameTo(new File(CONFIG_FILE));
-      }
-      File configFile = new File(CONFIG_FILE);
-      // If config file does not exists creates a new one with the default values
-      configFile.createNewFile();
-
       PropertiesCache cache = PropertiesCache.getInstance();
       if (cache.getProperty("radius") == null) {
         cache.setProperty("radius", DEF_RAD);
--- src/main/java/net/mobProofCrystals/util/PropertiesCache.java
+++ src/main/java/net/mobProofCrystals/util/PropertiesCache.java
@@ -1,5 +1,6 @@
 package net.mobProofCrystals.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -12,10 +13,19 @@ import java.util.Set;
 public class PropertiesCache {
   private final Properties configProp = new Properties();
   final public static String CONFIG_FILE = "config/crystal.properties";
+  final public static String DEPRECATED_CONFIG_FILE = "crystal.properties";
   private final String defaultConfig = "radius specifies the radius of the cube\nlower-limit-distance specifies the distance between the crystal and the lower border of the area\nset crystal-name to a value to make only renamed end crystals (with a nametag) to be be spawn proofing";
 
   private PropertiesCache() {
     try {
+      File deprecatedConfigFile = new File(DEPRECATED_CONFIG_FILE);
+      if (deprecatedConfigFile.isFile()) {
+        deprecatedConfigFile.renameTo(new File(CONFIG_FILE));
+      }
+      File configFile = new File(CONFIG_FILE);
+      // If config file does not exists creates a new one with the default values
+      configFile.createNewFile();
+
       InputStream in = new FileInputStream(CONFIG_FILE);
       configProp.load(in);
     } catch (IOException e) {
